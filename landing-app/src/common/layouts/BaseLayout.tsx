424dda783fa5ded4046fa0d50ac79c9382b7e7cc
Alvaro <alvaro.guillenc1612@gmail.com>
2023-09-19 16:17:23 -0500

    commitCard component
    commit card component styles and data display finished

--- landing-app/src/common/layouts/BaseLayout.tsx
+++ landing-app/src/common/layouts/BaseLayout.tsx
@@ -2,6 +2,8 @@ import { FC, PropsWithChildren } from "react";
 
 export const BaseLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="min-h-screen min-w-full p-4 bg-zinc-800">{children}</div>
+    <div className="min-h-screen  p-4 bg-zinc-800">
+      <div className="m-auto w-full md:w-5/6 lg:w-8/12">{children}</div>
+    </div>
   );
 };
--- landing-app/src/features/commit/components/CommitCard.tsx
+++ landing-app/src/features/commit/components/CommitCard.tsx
@@ -3,9 +3,20 @@ import { CommitInfo } from "../services/fetchAllCommits";
 export const CommitCard = ({ commit }: { commit: CommitInfo }) => {
   return (
     <div className="bg-zinc-900 p-4 rounded-md border border-zinc-700 shadow-md">
-      <h1 className="first-letter:uppercase text-stone-400">
-        {commit.commit.message}
-      </h1>
+      <div className="flex justify-between">
+        <h1 className="text-stone-400 font-bold flex">
+          {commit.sha.substring(0, 10)} -&nbsp;
+          <span className="inline-block first-letter:uppercase max-w-[120px] sm:max-w-[340px] lg:max-w-[400px] xl:max-w-none text-ellipsis overflow-hidden whitespace-nowrap">
+            {commit.commit.message}
+          </span>
+        </h1>
+        <div className="flex gap-4">
+          <p className="hidden sm:inline-block text-stone-400">
+            {commit.author.login}
+          </p>
+          <img src={commit.committer.avatar_url} className="w-6 rounded" />
+        </div>
+      </div>
     </div>
   );
 };
