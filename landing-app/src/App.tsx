14ba75d894aec4734697d3759eaf0693a54fb7c3
Alvaro <alvaro.guillenc1612@gmail.com>
2023-09-19 18:07:19 -0500

    cleaned up unused resources and logs

--- landing-app/src/App.tsx
+++ landing-app/src/App.tsx
@@ -2,7 +2,6 @@ import { BaseLayout } from "./common/layouts/BaseLayout";
 import { RepoLanding } from "./features/repo/pages/RepoLanding";
 
 function App() {
-  console.log("-----", import.meta.env);
   return (
     <BaseLayout>
       <RepoLanding />
