64020b5c5d8211d1bef34ba1ac270cd8c4470884
Alvaro <alvaro.guillenc1612@gmail.com>
2023-10-03 21:02:04 -0500

    done bucket url config

--- landing-app/src/env.ts
+++ landing-app/src/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
   API_URL: import.meta.env.VITE_API_URL,
   REPO: import.meta.env.VITE_REPO,
-};
+} as const;
--- landing-app/vite.config.ts
+++ landing-app/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import z, { ZodRawShape } from "zod";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ mode }) => {
+  const currentEnv = loadEnv(mode, process.cwd(), "");
+
+  const zodSchema: z.ZodObject<ZodRawShape> = z.object({
+    VITE_API_URL: z.string().url(),
+    BUCKET_URL: z.string().url().optional(),
+  });
+
+  zodSchema.parse(currentEnv);
+  console.log(currentEnv.BUCKET_URL);
+
+  return {
+    plugins: [react()],
+    experimental: {
+      renderBuiltUrl(filename) {
+        if (!currentEnv.BUCKET_URL) return filename;
+        const resultingFilename = currentEnv.BUCKET_URL + "/" + filename;
+        return resultingFilename;
+      },
+    },
+  };
+});
